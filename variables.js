c4a2788e3296f266ce47f7e062b47a7348533af6
maksitcous-gif <maksitcous@gmail.com>
2025-10-17 13:48:53 +0100

    javproject

---
+++ variables.js
@@ -0,0 +1,14 @@
+
+let result = calculation + 2;
+console.log(result);
+
+let message = 'hello';
+console.log(message);
+// changing let variable value, to change it we dont 
+// need to let again//
+variable1 = 5;
+console.log(variable1);
+
+
+variable1 = variable1 + 1;
+console.log(variable1);
